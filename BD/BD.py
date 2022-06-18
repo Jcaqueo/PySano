1a7b57ab366cb2e9e02da5b456c4e17145876663
Jcaqueo <joaquin@DESKTOP-N38J4L6.localdomain>
2022-06-17 20:54:47 -0400

    Search Student

--- BD/BD.py
+++ BD/BD.py
@@ -1,26 +1,30 @@
 import psycopg2
 
+
 def connect():
     try:
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect("dbname=PySano user=postgres password=admin host=127.0.1.1")
-        print('Conection Successful')
+        print('Conectando a la base de datos PostgreSQL...')
+        conn = psycopg2.connect(
+            "dbname=PySano user=postgres password=admin host=127.0.1.1")
+        print('Conexion exitosa')
         return conn
     except Exception as error:
         print(error)
 
-def uploadStudent(student,values):
+
+def uploadStudent(student, values):
     try:
-        #Realizamos una conexion y obtenemos el cursor
+        # Realizamos una conexion y obtenemos el cursor
         conn = connect()
         cursor = conn.cursor()
-        rol = student 
-        Uvas =  values
+        rol = student
+        Uvas = values
         Uvas += [0]*(9-len(Uvas))
         table = '"Student"'
         query = 'INSERT INTO %s ("Rol", "Uva1", "Uva2", "Uva3", "Uva4", "Uva5", "Uva6", "Uva7", "Uva8", "Uva9") VALUES (%%s, %%s, %%s, %%s, %%s, %%s, %%s, %%s, %%s, %%s)' % table
-        argsTuple = (rol, Uvas[0], Uvas[1], Uvas[2], Uvas[3], Uvas[4], Uvas[5], Uvas[6], Uvas[7], Uvas[8])
-        cursor.execute(query,argsTuple)
+        argsTuple = (rol, Uvas[0], Uvas[1], Uvas[2], Uvas[3],
+                     Uvas[4], Uvas[5], Uvas[6], Uvas[7], Uvas[8])
+        cursor.execute(query, argsTuple)
         conn.commit()
         conn.close()
         cursor.close()
@@ -28,24 +32,31 @@ def uploadStudent(student,values):
     except Exception as error:
         print(error)
 
+
 def getStudent(student):
-    #Intentamos obtener el estudiante por rol 
+    # Intentamos obtener el estudiante por rol
     try:
-        #Realizamos una conexion y obtenemos el cursor
+        # Realizamos una conexion y obtenemos el cursor
         conn = connect()
         cursor = conn.cursor()
-        rol = student 
+        rol = student
         table = "Student"
-        query = 'Select * from %s where "Rol" =  %s'
-        print(query)
-        argsTuple = (table,rol)
-        ans = cursor.execute(query,argsTuple)
-        print(ans)
+        query = f'Select * from "{table}" where "Rol" =  %s'
+        argsTuple = (rol,)
+        cursor.execute(query, argsTuple)
+        ans = cursor.fetchall()
+
         conn.commit()
         conn.close()
         cursor.close()
+
+        if ans != []:
+            print("Usuario "+ans[0][0]+" encontrado exitosamente.")
+            return ans[0]
+        else:
+            print("Usuario no encontrado en la base de datos")
+            return None
+
     except Exception as error:
         print(error)
-
-getStudent("201873081-5")
 
